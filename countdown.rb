1ccea4ff2769d310546e6796ecfd7146699c1242
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 02:59:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,32 +1,31 @@
 #write your code here
 
-# def countdown(n)
-#   while n > 0
-#     if n == 1
-#     puts "#{n} SECOND!"
-#     n -= 1
-#     else
-#       puts "#{n} SECONDS!"
-#       n -= 1
-#     end
-#   "HAPPY NEW YEAR!"
-#   end
-# end
+def countdown(n)
+  while n > 0
+    if n == 1
+    puts "#{n} SECOND!"
+    n -= 1
+    else
+      puts "#{n} SECONDS!"
+      n -= 1
+    end
+  "HAPPY NEW YEAR!"
+  end
+end
 
-# def countdown_with_sleep(n)
-#   while n > 0
-#     if n == 1
-#       puts "#{n} SECOND!"
-#       sleep(1)
-#       n -= 1
-#     else
-#       puts "#{n} SECONDS!"
-#       sleep(1)
-#       n -= 1
-#     end
-#   end
-#   puts "HAPPY NEW YEAR!"
-# end
-#
-# countdown_with_sleep(5)
+def countdown_with_sleep(n)
+  while n > 0
+    if n == 1
+      puts "#{n} SECOND!"
+      sleep(1)
+      n -= 1
+    else
+      puts "#{n} SECONDS!"
+      sleep(1)
+      n -= 1
+    end
+  end
+  puts "HAPPY NEW YEAR!"
+end
 
+countdown_with_sleep(5)
