f884cd4070189b7b7fa57c114aab6c0d426f3eea
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 02:59:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -30,19 +30,3 @@
 #
 # countdown_with_sleep(5)
 
-def countdown(n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    sleep(1)
-    n -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
